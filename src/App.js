b53b7cd3ec5b2f6c898551ccceca4c080ba63eb9
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-13 12:33:47 +0600

    login page design complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Register from './Pages/Register/Register';
 import Navbar from './Pages/Shared/Navbar/Navbar';
+import Login from './Pages/Login/Login';
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Routes>
+        <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
 
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Login = () => {
+
+    const handleLogin = (event) => {
+
+        event.preventDefualt()
+
+        const password = event.target.password.value;
+        const email = event.target.email.value;
+        console.log(email, password)
+
+    }
+    return (
+        <div className='flex justify-center items-center mb-10'>
+            <div className="card w-96 bg-gray-200 shadow-xl">
+                <div className="card-body ">
+                    <h2 className="text-center text-2xl font-bold">Login</h2>
+
+
+                    <form onSubmit={handleLogin} >
+
+
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+
+                            </label>
+
+                            <input type="email"
+                                placeholder="Email Address"
+                                name="email"
+                                className="input input-bordered w-full max-w-xs"
+
+                            />
+
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+
+                            </label>
+
+                            <input type="password"
+                                placeholder="Password"
+                                name="password"
+                                className="input input-bordered w-full max-w-xs"
+                            />
+                        </div>
+
+                        <input className='btn  w-full max-w-xs' type="submit" value="Login" />
+                    </form>
+
+                    <p className='text-lg'><small>Don't Have Account? <Link className='text-error font-semibold' to='/register'>Register</Link ></small></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
